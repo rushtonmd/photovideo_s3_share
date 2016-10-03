d46d3be468b511d57dfa859e2a46b935464a2fc8
Mark D Rushton <rushtonmd@github.com>
2016-10-02 21:30:38 -0700

    test commit

--- imports/startup/server/browser-policies.js
+++ imports/startup/server/browser-policies.js
@@ -3,6 +3,12 @@ BrowserPolicy.content.allowOriginForAll( '*.s3.amazonaws.com' );
 BrowserPolicy.content.allowOriginForAll( '*.s3-us-west-1.amazonaws.com' );
 BrowserPolicy.content.allowOriginForAll( '*.s3-us-west-2.amazonaws.com' );
 
+BrowserPolicy.content.allowOriginForAll('https://s3-us-west-2.amazonaws.com');
+BrowserPolicy.content.allowEval('https://s3-us-west-2.amazonaws.com');
+BrowserPolicy.content.allowScriptOrigin('https://s3-us-west-2.amazonaws.com');
+BrowserPolicy.content.allowImageOrigin('https://s3-us-west-2.amazonaws.com');
+
+
 //Blob URLS (camera)
 BrowserPolicy.content.allowImageOrigin("blob:");
 var constructedCsp = BrowserPolicy.content._constructCsp();
